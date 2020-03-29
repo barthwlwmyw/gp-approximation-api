b8095ffa117807793fe3413ab2de53f61455ed97
barthwlwmyw <bartlomiejkacperski7734@gmail.com>
2020-03-29 21:52:52 +0200

    Add project files.

---
+++ gp-approximation-api/Controllers/SessionController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using gp_approximation_api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gp_approximation_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SessionController : ControllerBase
+    {
+        private readonly GpApproximationManager _gpApproximationManager;
+
+        public SessionController(GpApproximationManager gpApproximationManager)
+        {
+            _gpApproximationManager = gpApproximationManager;
+        }
+
+        [HttpGet]
+        public IActionResult GetGuid()
+        {
+            var guid = Guid.NewGuid();
+
+            GpApproximationManager.ApproximationTasks.Add(new ApproximationTask { TaskGuid = guid, TaskProgress = 0 });
+
+            return Ok(new { SessionGuid = guid });
+        }
+        [HttpGet]
+        [Route("/all")]
+        public IActionResult GetAllTasks()
+        {
+            return Ok(new { Tasks = GpApproximationManager.ApproximationTasks });
+        }
+    }
+}
---
+++ gp-approximation-api/Services/GpApproximationManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gp_approximation_api.Services
+{
+    public class GpApproximationManager
+    {
+        public GpApproximationManager()
+        {
+            ApproximationTasks = new List<ApproximationTask>();
+        }
+
+        public static List<ApproximationTask> ApproximationTasks { get; set; }
+
+        public static void UpdateTaskProgress(Guid taskGuid, int progress)
+        {
+            ApproximationTasks.Where(at => at.TaskGuid == taskGuid).FirstOrDefault().TaskProgress = progress;
+        }
+    }
+
+    public class ApproximationTask
+    {
+        public Guid TaskGuid { get; set; }
+        public int TaskProgress { get; set; }
+    }
+}
